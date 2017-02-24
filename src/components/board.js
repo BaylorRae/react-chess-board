23c066f73a4b14752864ba58376b16692a622e77
Baylor Rae <baylor@thecodedeli.com>
2017-02-23 20:56:06 -0600

    begin adding board

---
+++ src/components/board.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import '../stylesheets/components/board.css';
+
+class Board extends Component {
+  render() {
+    return (
+      <div className="board">
+      </div>
+    );
+  }
+}
+
+export default Board;
